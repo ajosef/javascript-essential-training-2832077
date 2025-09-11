6df2c0574de71b9673c5e7d1249eeb1426da67b2
ajosef <36704337+ajosef@users.noreply.github.com>
2025-09-11 15:49:19 +0530

    Modified,

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,16 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+import Room from "./Room.js";
+
+const bedRoom = new Room("bedroom1", 1, true, 2, 1, true, 1, 8, 2, 0, 1);
+globalThis.bedRoom = bedRoom;
+
+console.log(bedRoom);
+console.log(bedRoom.numWindows());
+
+//const livingRoom = new Room("livingRoom", 1, true, 2, 1, true, 1, 8, 2, 0, 1);
+const livingRoom = new Room("livingRoom", 1, false, 1, 0, false, 0, 2, 0, 0, 2);
+globalThis.livingRoom = livingRoom;
+console.log(livingRoom);
+console.log(livingRoom.numWindows());
