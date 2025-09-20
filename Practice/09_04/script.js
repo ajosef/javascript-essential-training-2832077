1fb5eacd9630fc79af4379f843b7196b66c91fd8
ajosef <36704337+ajosef@users.noreply.github.com>
2025-09-20 16:39:00 +0530

    Practice 09_04 - implement grid event listeners.

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,59 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+//Use an event listener & CSS to highlight around the entire grid when you mouse over.
+const grid = document.querySelector(".grid");
+grid.addEventListener(
+  "pointerenter",
+  (e) => {
+    //console.log(e);
+    grid.style.outline = "10px solid lime";
+  },
+  false
+);
+grid.addEventListener(
+  "pointerleave",
+  (e) => {
+    //console.log(e);
+    grid.style.outline = "";
+  },
+  false
+);
+
+//Add an event listener to each grid cell to highlight that cell when you hover your mouse over it.
+const borderCell = (event) => {
+  event.target.style.outline = "5px solid green";
+};
+
+const unBorderCell = (event) => {
+  event.target.style.outline = "";
+};
+
+// Add an event listener to each grid cell to change its background color when it is clicked.
+const cellClicked = (event) => {
+  event.target.classList.toggle("clicked");
+};
+
+document.querySelectorAll(".cell").forEach((cell) => {
+  cell.addEventListener("pointerenter", borderCell, false);
+  cell.addEventListener("pointerleave", unBorderCell, false);
+  cell.addEventListener("click", cellClicked, false);
+});
+
+//Add an event listener to a specific key on the keyboard
+// to change the background color of the whole page - from dark to light and back again.
+const body = document.body;
+body.addEventListener(
+  "keyup",
+  (event) => {
+    //console.log(event, event.key);
+    switch (event.key) {
+      case "A":
+      case "a":
+        body.style.backgroundColor =
+          body.style.backgroundColor === "" ? "darkolivegreen" : "";
+    }
+  },
+  false
+);
