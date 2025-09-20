23316c1dfdbf75c9bd4ed21ba5a5695f227a5277
ajosef <36704337+ajosef@users.noreply.github.com>
2025-09-20 16:42:53 +0530

    Improvement

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -18,7 +18,7 @@ const chair1 = {
   color: "green",
 };
 const chair2 = {
-  id: 1,
+  id: 2,
   name: "chair2",
   type: "easychair",
   size: "large",
@@ -31,21 +31,21 @@ const chair2 = {
  * @param {*} chair object with all properties of chair
  * @returns HTMLElement having ul/li of chair data.
  */
-function newChair(chair) {
+function newChair1(chair) {
   console.log("fn declaration");
-  const chairE = document.createElement("chair");
-  chairE.innerHTML = `<h3>${chair.name}</h3>
-  <ul>
-    <li>id : ${chair.id} </li>
-    <li>type : ${chair.type} </li>
-    <li>material : ${chair.material} </li>
-    <li>color : ${chair.color} </li>
-  </ul>`;
-  chairE.style.color = chair.color;
-  return chairE;
+  // const chairE = document.createElement("chair");
+  // chairE.innerHTML = `<h3>${chair.name}</h3>
+  // <ul>
+  //   <li>id : ${chair.id} </li>
+  //   <li>type : ${chair.type} </li>
+  //   <li>material : ${chair.material} </li>
+  //   <li>color : ${chair.color} </li>
+  // </ul>`;
+  // return chairE;
+  return newChair(chair);
 }
 
-const newChair2 = function (chair) {
+const newChair = function (chair) {
   console.log("fn expression");
   const chairE = document.createElement("chair");
   chairE.innerHTML = `<h3>${chair.name}</h3>
@@ -56,18 +56,34 @@ const newChair2 = function (chair) {
     <li>color : ${chair.color} </li>
   </ul>`;
   chairE.style.color = chair.color;
+  chairE.setAttribute("data-id", chair.id);
+  chairE.setAttribute("data-name", chair.name);
   return chairE;
 };
 
 const addChair = (chair) => {
   console.log("fn arrow");
-  const chairE = newChair2(chair);
+  const chairE = newChair(chair);
   let mainE = document.querySelector("main");
   mainE.append(chairE);
   return chairE;
 };
 
+const getChairByDataAttr = (attr, val) => {
+  return document.querySelector(`chair[data-${attr}="${val}"]`);
+};
+
+const getElemByAttr = (tag, attr, val) => {
+  return document.querySelector(`${tag}[${attr}="${val}"]`);
+};
+
+const removeChairByDataAttr = (attr, val) => {
+  let child = document.querySelector(`chair[data-${attr}="${val}"]`);
+  child ? child.remove() : null;
+  return child;
+};
+
 const mainE = document.querySelector("main");
 mainE.innerHTML = `<h2> Chairs </h2>`;
-mainE.append(newChair2(chair1));
+mainE.append(newChair(chair1));
 //mainE.append(newChair2(chair2));
